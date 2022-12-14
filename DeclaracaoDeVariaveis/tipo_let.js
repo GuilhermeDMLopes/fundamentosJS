e861ceb5aeba911adcc8e1f732a8b58699b1598d
guilherme <guilhermedmlopes@hotmail.com>
2022-12-13 21:06:23 -0300

    Adding info about let

--- DeclaracaoDeVariaveis/tipo_let.js
+++ DeclaracaoDeVariaveis/tipo_let.js
@@ -21,3 +21,18 @@ console.log(area)
 
 //Mas ainda sim podemos mudar os valores das variáveis dentro do código.
 //Se tivessemos inicializado let area = 0, o valor seria alterado de acordo com a formula.
+
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
+
+// Como observado, a saída é 3.14 42 29 3.14 29 3.14. O let só é alterado quando mudamos no mesmo bloco de código
